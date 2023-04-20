25ddf7d66b5a7b342cf99c0291f0a49e5917cbb3
Michael van der Luit <3525053+mvdluit@users.noreply.github.com>
2023-04-20 19:47:43 +0200

    initialize carousel

--- Piadineria-App/src/app/components/home/home.component.ts
+++ Piadineria-App/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    $(document).ready(function () {
+      $('.owl-carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+      });
+    });
+  }
 }
